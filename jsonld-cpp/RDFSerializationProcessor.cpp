0469bd27742bbee31a7246ba320c7a185f62a5ff
Daniel X. Pape <dpape@dpape.com>
2022-12-15 16:09:45 -0800

    use json::dump() to mostly implement @json datatype handling
    fixes several toRDF tests tjs*, but breaks tjs04

--- jsonld-cpp/RDFSerializationProcessor.cpp
+++ jsonld-cpp/RDFSerializationProcessor.cpp
@@ -467,6 +467,7 @@ namespace {
         // If datatype is @json, convert value to the canonical lexical form using the result
         // of transforming the internal representation of value to JSON and set datatype to rdf:JSON.
         if(datatypeStr == JsonLdConsts::JSON) {
+            value = value.dump();
             datatypeStr = JsonLdConsts::RDF_JSON;
         }
 
