2ebb7827c588d18b85a363372825a0074e084df0
Daniel X. Pape <dpape@dpape.com>
2023-12-11 19:57:06 -0800

    change Permutator implementation to use std lib

--- jsonld-cpp/Permutator.h
+++ jsonld-cpp/Permutator.h
@@ -5,13 +5,10 @@
 #include <map>
 #include <string>
 
-// Thanks to jsonld-java for this class
-// todo: make sure to copy license nd any header info
 
 class Permutator {
 private:
     std::vector<std::string> strings;
-    std::map<std::string, bool> left;
     bool done;
 
 public:
